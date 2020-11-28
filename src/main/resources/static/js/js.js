5c21de5336a6ecfd41ca57833477fd9a057c0f7e
kali <primat.2000@mail.ru>
2020-11-28 15:40:16 +0500

    kk

--- src/main/resources/static/js/js.js
+++ src/main/resources/static/js/js.js
@@ -33,3 +33,41 @@
 //     });
 //     getrole();
 // }
+
+function getrole() {
+    axios.get("/getrole").then(function (response) {
+        // console.log(response);
+        if(response.data==="USER"){
+            document.getElementById("kirish").href="/user";
+            document.getElementById("kirish").textContent="Kabinet"
+            // document.getElementById("kabinet").style.display="block";
+        }
+        else {
+            if (response.data==="ADMIN"){
+                document.getElementById("kirish").href="/admin";
+                document.getElementById("kirish").textContent="Kabinet"
+            } else {
+                document.getElementById("kirish").href="/login";
+                document.getElementById("kirish").textContent="Kirish"
+            }
+        }
+
+    });
+}
+
+// function getusername() {
+//     let anonim= "<a href='/login' class=\"btn-success btn mt-5\">Kirish</a>";
+//     let user= "<a href='/logout' class=\"btn-success btn mt-5\">Chiqish</a>";
+//     axios.get("/getusername").then(function (response) {
+//         // console.log(response.data);
+//         if (response.data==="anonymousUser"){
+//             // document.getElementById("btns").innerHTML=anonim;
+//         }
+//         else {
+//             document.getElementById("btns").innerHTML=user;
+//             document.getElementById("name").innerHTML="Xush kelibsiz "+response.data;
+//         }
+//         // document.getElementById("un").innerHTML=response.data;
+//     });
+//     getrole();
+// }

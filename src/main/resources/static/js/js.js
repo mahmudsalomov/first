a91435c3a9354871bf4e9e9e8f828679b51ddc1b
kali <primat.2000@mail.ru>
2020-11-28 15:31:03 +0500

    kk

--- src/main/resources/static/js/js.js
+++ src/main/resources/static/js/js.js
@@ -1,35 +1,35 @@
-function getrole() {
-    axios.get("/getrole").then(function (response) {
-        // console.log(response);
-        if(response.data==="USER"){
-            document.getElementById("kabinet").href="/user";
-            document.getElementById("kabinet").style.display="block";
-        }
-        else {
-            if (response.data==="ADMIN"){
-                document.getElementById("kabinet").href="/admin";
-                document.getElementById("kabinet").style.display="block";
-            } else {
-
-            }
-        }
-
-    });
-}
-
-function getusername() {
-    let anonim= "<a href='/login' class=\"btn-success btn mt-5\">Kirish</a>";
-    let user= "<a href='/logout' class=\"btn-success btn mt-5\">Chiqish</a>";
-    axios.get("/getusername").then(function (response) {
-        // console.log(response.data);
-        if (response.data==="anonymousUser"){
-            // document.getElementById("btns").innerHTML=anonim;
-        }
-        else {
-            document.getElementById("btns").innerHTML=user;
-            document.getElementById("name").innerHTML="Xush kelibsiz "+response.data;
-        }
-        // document.getElementById("un").innerHTML=response.data;
-    });
-    getrole();
-}
+// function getrole() {
+//     axios.get("/getrole").then(function (response) {
+//         // console.log(response);
+//         if(response.data==="USER"){
+//             document.getElementById("kabinet").href="/user";
+//             document.getElementById("kabinet").style.display="block";
+//         }
+//         else {
+//             if (response.data==="ADMIN"){
+//                 document.getElementById("kabinet").href="/admin";
+//                 document.getElementById("kabinet").style.display="block";
+//             } else {
+//
+//             }
+//         }
+//
+//     });
+// }
+//
+// function getusername() {
+//     let anonim= "<a href='/login' class=\"btn-success btn mt-5\">Kirish</a>";
+//     let user= "<a href='/logout' class=\"btn-success btn mt-5\">Chiqish</a>";
+//     axios.get("/getusername").then(function (response) {
+//         // console.log(response.data);
+//         if (response.data==="anonymousUser"){
+//             // document.getElementById("btns").innerHTML=anonim;
+//         }
+//         else {
+//             document.getElementById("btns").innerHTML=user;
+//             document.getElementById("name").innerHTML="Xush kelibsiz "+response.data;
+//         }
+//         // document.getElementById("un").innerHTML=response.data;
+//     });
+//     getrole();
+// }
